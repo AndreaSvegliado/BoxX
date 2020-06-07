bc1531b4dfb05814db6feecfdb48ce2924330adf
Andrea Svegliado <andrea.svegliado@gmail.com>
2020-06-07 20:19:24 +0200

    07/06/20

--- src/app/components/tickets/ticket-detail-list/ticket-detail-list.component.ts
+++ src/app/components/tickets/ticket-detail-list/ticket-detail-list.component.ts
@@ -20,19 +20,16 @@ export class TicketDetailListComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private router : Router, public ticketDetailService: TicketDetailService ) { 
 
-    let ID = this.route.snapshot.params['ID'];
-    //this.ticketID = ID;
-
     this.ticketID = this.route.snapshot.params['ID'];
-    console.log("ANDREA");
-    console.log(this.ticketID);
-
+  
+    /*
     this.ticketDetailService.getTicketDetailList(this.ticketID)
       .subscribe(
         res=>   {this.ticketDetails = res as ticketDetail[];
         console.log ('aggiornamento in corso...');
         }
     );  
+    */
   }
 
   ngOnInit(): void {
@@ -41,12 +38,9 @@ export class TicketDetailListComponent implements OnInit {
 
   
   populateForm(objTicketDetail: ticketDetail){
-    //this.ticketDetailService.formData=objTicketDetail;
-    
     //per evitare che i dati del form aggiornino direttamente la griglia prima del POST:
     //assegno un nuovo oggetto clonato da quello passato come parametro
     this.ticketDetailService.formData= Object.assign({}, objTicketDetail);  
-
   }
   
   onDelete(id: BigInteger){
@@ -54,7 +48,6 @@ export class TicketDetailListComponent implements OnInit {
       this.ticketDetailService.deleteTicketDetail(id)
         .subscribe(res => {
         this.ticketDetailService.refreshList(this.ticketID);
-        
         //this.toastr.warning('Record cancellato', 'Payment Detail Register');
       },
       err=> {
