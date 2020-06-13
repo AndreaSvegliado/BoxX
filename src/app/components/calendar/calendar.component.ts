6383f8fff840beaa2730b227f43ce2560c629274
nicolacardi <nicola.cardi@gmail.com>
2020-06-13 22:20:48 +0200

    fc events

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -29,17 +29,41 @@ export class CalendarComponent implements OnInit {
   //ecco un evento
   impostaData1 = new Date('Wed Jun 02 2020 00:01:00 GMT+0100');
   impostaData2 = new Date('Wed Jun 02 2020 00:02:00 GMT+0100');
+  impostaData3 = new Date('Wed Jun 17 2020 00:01:00 GMT+0100');
+  impostaData4 = new Date('Wed Jun 17 2020 00:02:00 GMT+0100');
+  impostaData5 = new Date('Wed Jun 29 2020 00:01:00 GMT+0100');
+  impostaData6 = new Date('Wed Jun 29 2020 00:02:00 GMT+0100');
   calendarEvents= 
-    [
+  [
       {
         allDay: true,
-        color: '#7fff64',
+        color: '#00bb99',
         date: '2020-06-02',
         end: this.impostaData2,
         id: "1",
         start: this.impostaData1,
-        textColor: "#000",
-        title: 'Dentista'
+        textColor: "#fff",
+        title: 'qui devono vedersi i ticket'
+      },
+      {
+        allDay: true,
+        color: '#ffcc00',
+        date: '2020-06-17',
+        end: this.impostaData3,
+        id: "1",
+        start: this.impostaData4,
+        textColor: "#555",
+        title: 'in vari colori e draggabili'
+      },
+      {
+        allDay: true,
+        color: '#ff5500',
+        date: '2020-06-29',
+        end: this.impostaData5,
+        id: "1",
+        start: this.impostaData6,
+        textColor: "#fff",
+        title: 'anche editabili? se sì serve modalform'
       },
   ];
 
